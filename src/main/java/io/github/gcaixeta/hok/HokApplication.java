77f0e79f9e44a6f36cf88c022d84cbf985620d63
Gustavo Caixeta Rosa <gstvcaixeta@gmail.com>
2026-08-25 22:05:28 -0300

    init project

---
+++ src/main/java/io/github/gcaixeta/hok/HokApplication.java
@@ -0,0 +1,13 @@
+package io.github.gcaixeta.hok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/gcaixeta/hok/HokApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gcaixeta.hok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
