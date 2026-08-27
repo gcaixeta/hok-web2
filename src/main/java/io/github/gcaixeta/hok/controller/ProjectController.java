d079800d60a5184f9e8d6e5493918497becd1c2d
Gustavo Caixeta Rosa <gstvcaixeta@gmail.com>
2026-08-26 21:29:23 -0300

    first controller and template

---
+++ src/main/java/io/github/gcaixeta/hok/controller/ProjectController.java
@@ -0,0 +1,15 @@
+package io.github.gcaixeta.hok.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+  @GetMapping
+  public String index() {
+    return "project/index";
+  }
+}
